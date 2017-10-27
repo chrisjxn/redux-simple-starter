c30f14dc6366ed633e734f6d9b547cf96f529895
chrisjxn <usjacksons@gmail.com>
2017-10-26 19:18:56 -0600

    gitignore api_key

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 import React from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -7,8 +6,13 @@ import YTSearch from 'youtube-api-search';
 // import registerServiceWorker from './registerServiceWorker';
 
 import SearchBar from './components/search_bar';
+import YT_API_KEY from './api_key/api_key';
 
-const API_KEY = process.env.API_KEY
+const API_KEY = YT_API_KEY;
+
+YTSearch({key: API_KEY, term: 'chris harris drives'}, function(data) {
+    console.log(data);
+});
 
 
 
