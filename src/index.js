8debfa8969e73e5a9d41b4c87a2fbc18080e5fa1
chrisjxn <usjacksons@gmail.com>
2017-10-26 18:49:08 -0600

    regen API and move to dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 // import './index.css';
 // import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = process.env.API_KEY
+
+
+
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
